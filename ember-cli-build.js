4c4d2f8203015b8275c5077689af6e5be96914a6
Peter Janak <2010janak2010@gmail.com>
2016-10-18 03:01:36 +0100

    chore: Install sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,18 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: 'sass',
+      includePaths: [
+        'bower_components/material-design-lite/src'
+      ],
+      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
