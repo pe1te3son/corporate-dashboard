ff4d4704236d19ecc26863fe83fd38fa7727ada0
Peter Janak <2010janak2010@gmail.com>
2016-10-19 04:24:35 +0100

    style: Lint with semi-standard

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,17 +1,17 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var nodeSass = require('node-sass'); // loads the version in your package.json
+var EmberApp = require("ember-cli/lib/broccoli/ember-app");
+var nodeSass = require("node-sass"); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
     sassOptions: {
-      extension: 'sass',
+      extension: "sass",
       includePaths: [
-        'bower_components/material-design-lite/src'
+        "bower_components/material-design-lite/src"
       ],
-      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
+      nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
     }
   });
 
@@ -20,13 +20,13 @@ module.exports = function(defaults) {
   //
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
+  // object"s keys should be the environment name and the values
   // should be the asset to use in that environment.
   //
   // If the library that you are including contains AMD or ES6
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/material-design-lite/material.js');
+  app.import("bower_components/material-design-lite/material.js");
   return app.toTree();
 };
