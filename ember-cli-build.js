8c7ec1af9662fb5ef64daa6d93a0e289bb033618
Peter Janak <2010janak2010@gmail.com>
2016-10-25 02:32:27 +0100

    feat: Add google charts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -28,5 +28,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import("bower_components/material-design-lite/material.js");
+  app.import("vendor/google_charts.js");
   return app.toTree();
 };
