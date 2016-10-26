177c2e3f015c9c934db27722271093ec9f235d59
Peter Janak <2010janak2010@gmail.com>
2016-10-26 03:33:03 +0100

    chore: Cleaning up

--- app/controllers/issues-page.js
+++ app/controllers/issues-page.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init () {
-    Ember.run.schedule('afterRender', () => {
-      // console.log(this.get('model'));
-    });
-  }
 });
