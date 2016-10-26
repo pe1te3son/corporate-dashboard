1c131acd59643d2736edf4305cea499feb315891
Peter Janak <2010janak2010@gmail.com>
2016-10-26 03:15:21 +0100

    refactor: Use passed csv data with geo chart

--- app/components/geo-graph.js
+++ app/components/geo-graph.js
@@ -3,27 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement () {
     const elId = this.$().attr('id');
-    google.charts.load('upcoming', {'packages':['geochart']});
-     google.charts.setOnLoadCallback(drawRegionsMap);
+    const geoData = this.get('data');
+    google.charts.load('upcoming', {'packages': ['geochart']});
+    google.charts.setOnLoadCallback(drawRegionsMap);
 
-     function drawRegionsMap() {
+    function drawRegionsMap () {
+      const data = google.visualization.arrayToDataTable(dataArray);
+      const options = {};
+      const chart = new google.visualization.GeoChart(document.getElementById(elId));
 
-       var data = google.visualization.arrayToDataTable([
-         ['Country', 'Popularity'],
-         ['Germany', 200],
-         ['France', 300],
-         ['Spain', 400],
-         ['Poland', 500],
-         ['Belgium', 700]
-       ]);
-
-       var options = {
-         region: 150
-       };
-
-       var chart = new google.visualization.GeoChart(document.getElementById(elId));
-
-       chart.draw(data, options);
-     }
+      chart.draw(data, options);
+    }
   }
 });
