0b70565b580df2f17fc5905ce7f465615e40aa40
Peter Janak <2010janak2010@gmail.com>
2016-10-26 04:29:18 +0100

    refactor: Moved data convertion for geo-graph to parent controller
    Now passing formated data to components. Keeps the code clean

--- app/components/geo-graph.js
+++ app/components/geo-graph.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  dataConverter: Ember.inject.service('data-converter'),
 
   didInsertElement () {
     const elId = this.$().attr('id');
     const geoData = this.get('data');
-    const dataArray = this.get('dataConverter').csvToArray(geoData);
+    console.log(geoData);
+    if (!geoData) { return; }
     google.charts.load('upcoming', {'packages': ['geochart']});
     google.charts.setOnLoadCallback(drawRegionsMap);
 
     function drawRegionsMap () {
-      const data = google.visualization.arrayToDataTable(dataArray);
+      const data = google.visualization.arrayToDataTable(geoData);
       const options = {};
       const chart = new google.visualization.GeoChart(document.getElementById(elId));
 
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  dataConverter: Ember.inject.service('data-converter'),
+
+  init () {
+    Ember.run.schedule('afterRender', () => {
+      const data = this.get('model');
+      const dataArray = this.get('dataConverter').csvToArray(data);
+      this.set('dataArray', dataArray);
+      this.set('numberOfEmployees', this.getNumberOfEmployees(dataArray));
+    });
+  },
+
 });
