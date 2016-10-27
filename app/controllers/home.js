fb5ae6f1864449823c6b2a2956c7e1d07e763627
Peter Janak <2010janak2010@gmail.com>
2016-10-27 03:22:45 +0100

    refactor: Get api key from config file in home page controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
   dataConverter: Ember.inject.service('data-converter'),
   numberOfEmployees: 0,
 
   init () {
+    this.set('apiKey', config.dashboardAppGoogleApiKey);
+
     Ember.run.schedule('afterRender', () => {
       const data = this.get('model');
       const dataArray = this.get('dataConverter').csvToArray(data);
