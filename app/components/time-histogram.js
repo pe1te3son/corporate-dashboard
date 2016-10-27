0986462c8eccb7f39e0eeefe250ca2936763b775
Peter Janak <2010janak2010@gmail.com>
2016-10-27 03:23:23 +0100

    feat: Add histogram basic set up to time-histogram component

--- app/components/time-histogram.js
+++ app/components/time-histogram.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement () {
+    const elId = this.$().attr('id');
+    const dataArray = this.get('data');
+    google.charts.load("current", {packages: ["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart () {
+      var data = google.visualization.arrayToDataTable(dataArray);
+
+      var options = {
+        title: 'Lengths of dinosaurs, in meters',
+        legend: {position: 'none'},
+      };
+
+      var chart = new google.visualization.Histogram(document.getElementById(elId));
+      chart.draw(data, options);
+    }
+  }
 });
