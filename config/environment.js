66e701b99f736e814559938cb7b15301b7f9fb3f
Peter Janak <2010janak2010@gmail.com>
2016-10-27 03:26:04 +0100

    feat: Set google api key as environment variable in config file

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment,
     rootURL: "/",
     locationType: "auto",
+    dashboardAppGoogleApiKey: "AIzaSyCjI_zCU8lbou6wHIq66iZ8t-yFp2UWh4c",
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
