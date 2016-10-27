69f2c118cdc784ceaad5e3599927e7ee533097b3
Peter Janak <2010janak2010@gmail.com>
2016-10-27 03:25:33 +0100

    feat: Fetch data for issue route async using promises

--- app/routes/issues-page.js
+++ app/routes/issues-page.js
@@ -1,4 +1,34 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
+
+  model () {
+    let allData = {};
+    const customersPerYear = new Promise((resolve, reject) => {
+      $.get('data/customers_per_year.csv')
+        .done((data) => {
+          allData.customers = data;
+          resolve();
+        })
+        .fail(() => {
+          reject('Failded to fetch chart data');
+        });
+    });
+
+    const issuesPerYear = new Promise((resolve, reject) => {
+      $.get('data/issues_per_year.csv')
+        .done((data) => {
+          allData.issues = data;
+          resolve();
+        })
+        .fail(() => {
+          reject('Failded to fetch chart data');
+        });
+    });
+
+    return Promise.all([customersPerYear, issuesPerYear]).then(() => {
+      return allData;
+    });
+  }
 });
