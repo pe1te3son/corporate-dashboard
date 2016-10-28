216747dab463da10f1b4916124382135dcd9e558
Peter Janak <2010janak2010@gmail.com>
2016-10-28 03:38:39 +0100

    feat: Load first 10 issues to display

--- app/routes/issues-page.js
+++ app/routes/issues-page.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Route.extend({
+  model () {
+    return $.get('data/issues.json').then((data) => {
+      return data.slice(0, 10);
+    });
+  }
 });
