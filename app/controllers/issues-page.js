753476b83e2b6a338de1e8e42b1c0e03e63b9ef6
Peter Janak <2010janak2010@gmail.com>
2016-10-28 03:38:07 +0100

    feat: Moved issues table to its component

--- app/controllers/issues-page.js
+++ app/controllers/issues-page.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import moment from 'npm:moment';
 
 export default Ember.Controller.extend({
+  sortedModel: Ember.computed.sort('model', (a, b) => {
+    if (moment(a.date_submited).isBefore(b.date_submited)) {
+      return 1;
+    } else if (moment(a.date_submited).isAfter(b.date_submited)) {
+      return -1;
+    }
+    return 0;
+  })
 });
