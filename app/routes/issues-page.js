e5065c9b15d14d170aed80e9c1f84fba25aa52c4
Peter Janak <2010janak2010@gmail.com>
2016-10-31 02:01:03 +0000

    feat: Fetch all issues from store to issues-page

--- app/routes/issues-page.js
+++ app/routes/issues-page.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
-import $ from 'jquery';
 
 export default Ember.Route.extend({
   model () {
-    return $.get('data/issues.json').then((data) => {
-      return data.slice(0, 10);
+    return this.store.findAll('issue', {
+      startAt: 10,
+      endAt: 20
     });
   }
 });
