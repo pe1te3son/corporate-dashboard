6763fa28d6db70cac72b6ebbdec66c02eadf2df5
Peter Janak <2010janak2010@gmail.com>
2016-11-01 01:02:09 +0000

    feat: Set spinner on issues page load

--- app/routes/issues-page.js
+++ app/routes/issues-page.js
@@ -2,9 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.store.findAll('issue', {
-      startAt: 10,
-      endAt: 20
-    });
+    return this.store.findAll('issue');
+  },
+
+  actions: {
+    loading (transition, route) {
+      let controller = this.controllerFor('application');
+      controller.set('isLoading', true);
+
+      transition.finally(() => {
+        Ember.run.later(() => {
+          controller.set('isLoading', false);
+        }, 500);
+      });
+    }
   }
 });
