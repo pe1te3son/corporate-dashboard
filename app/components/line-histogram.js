d5198e764999b4aeea31ea6b2399e0bc717f7546
Peter Janak <2010janak2010@gmail.com>
2016-11-01 00:59:50 +0000

    feat: Use data passed into line histogram

--- app/components/line-histogram.js
+++ app/components/line-histogram.js
@@ -9,32 +9,23 @@ export default Ember.Component.extend({
   },
 
   didInsertElement () {
-    console.log(this.get('data'));
     const elId = this.$().attr('id');
 
     if (this.get('title')) {
       this.get('chartOptions').title = this.get('title');
     }
 
+    const formatedChartData = this.buildChartData(this.get('data'));
+
+    this.set('formatedChartData', formatedChartData);
+
     google.charts.load('current', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(() => {
       this.set('chartData', new google.visualization.DataTable());
-      this.get('chartData').addColumn('date', 'Time of Day');
-      this.get('chartData').addColumn('number', 'Rating');
+      this.get('chartData').addColumn('date', this.get('xAxisLegend'));
+      this.get('chartData').addColumn('number', this.get('yAxisLegend'));
 
-      this.get('chartData').addRows([
-        [new Date(2015, 0, 1), 5], [new Date(2015, 0, 2), 7], [new Date(2015, 0, 3), 3],
-        [new Date(2015, 0, 4), 1], [new Date(2015, 0, 5), 3], [new Date(2015, 0, 6), 4],
-        [new Date(2015, 0, 7), 3], [new Date(2015, 0, 8), 4], [new Date(2015, 0, 9), 2],
-        [new Date(2015, 0, 10), 5], [new Date(2015, 0, 11), 8], [new Date(2015, 0, 12), 6],
-        [new Date(2015, 0, 13), 3], [new Date(2015, 0, 14), 3], [new Date(2015, 0, 15), 5],
-        [new Date(2015, 0, 16), 7], [new Date(2015, 0, 17), 6], [new Date(2015, 0, 18), 6],
-        [new Date(2015, 0, 19), 3], [new Date(2015, 0, 20), 1], [new Date(2015, 0, 21), 2],
-        [new Date(2015, 0, 22), 4], [new Date(2015, 0, 23), 6], [new Date(2015, 0, 24), 5],
-        [new Date(2015, 0, 25), 9], [new Date(2015, 0, 26), 4], [new Date(2015, 0, 27), 9],
-        [new Date(2015, 0, 28), 8], [new Date(2015, 0, 29), 6], [new Date(2015, 0, 30), 4],
-        [new Date(2015, 0, 31), 6], [new Date(2015, 1, 1), 7], [new Date(2015, 1, 2), 9]
-      ]);
+      this.get('chartData').addRows(formatedChartData);
 
       this.drawChart(elId, this.get('chartData'), this.get('chartOptions'));
     });
