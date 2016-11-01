f6fb37a8ea2c96668e022ebfe5170b778ba9d6f6
Peter Janak <2010janak2010@gmail.com>
2016-11-01 01:01:09 +0000

    feat: Add model for number of paying customers

---
+++ tests/unit/models/recevied-customers-payment-test.js
@@ -0,0 +1,12 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('recevied-customers-payment', 'Unit | Model | recevied customers payment', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.ok(!!model);
+});
