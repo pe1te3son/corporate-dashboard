f80e96d4c0bfa8c0b1dd5f26a921c9d037056825
Peter Janak <2010janak2010@gmail.com>
2016-11-02 22:46:06 +0000

    refactor: Renamed line-histogram to google-chart component

--- tests/integration/components/google-chart-test.js
+++ tests/integration/components/google-chart-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('line-histogram', 'Integration | Component | line histogram', {
+moduleForComponent('google-chart', 'Integration | Component | google chart', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{line-histogram}}`);
+  this.render(hbs`{{google-chart}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#line-histogram}}
+    {{#google-chart}}
       template block text
-    {{/line-histogram}}
+    {{/google-chart}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
