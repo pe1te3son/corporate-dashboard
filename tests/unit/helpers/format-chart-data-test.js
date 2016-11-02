5c4b9ce75f7df5320c64f4c250dce3ec309e1f1a
Peter Janak <2010janak2010@gmail.com>
2016-11-02 22:35:08 +0000

    feat: Add chart data builder helper for google charts

---
+++ tests/unit/helpers/format-chart-data-test.js
@@ -0,0 +1,12 @@
+
+import { formatChartData } from 'corporate-dashboard/helpers/format-chart-data';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format chart data');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatChartData([42]);
+  assert.ok(result);
+});
+
