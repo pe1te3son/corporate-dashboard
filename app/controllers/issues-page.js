27850d11523e8ec38358a928f738b48b17ac5c8d
Peter Janak <2010janak2010@gmail.com>
2016-11-04 01:25:26 +0000

    feat: Issues are sort by date on init

--- app/controllers/issues-page.js
+++ app/controllers/issues-page.js
@@ -1,13 +1,27 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Controller.extend({
-  sortedModel: Ember.computed.sort('model', (a, b) => {
-    if (a.get('timestamp') < b.get('timestamp')) {
-      return 1;
-    } else if (a.get('timestamp') > b.get('timestamp')) {
-      return -1;
+  sortedModel: null,
+  sortAscending: false,
+
+  init () {
+    Ember.run.schedule('afterRender', () => {
+      this.sortStoreByDate();
+    });
+  },
+
+  sortStoreByDate (option) {
+    return this.store.findAll('issue').then(res => {
+      if (this.get('sortAscending')) {
+        return res.sortBy('timestamp');
+      }
+      return res.sortBy('timestamp').reverse();
+    }).then(sortedResponse => {
+      this.set('sortAscending', !this.get('sortAscending'));
+      this.set('sortedModel', sortedResponse);
+    });
+  },
     }
-    // if a === b
-    return 0;
-  }),
+  }
 });
