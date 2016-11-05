6c4303182be2581632f6b2291ac32de2c703ae66
Peter Janak <2010janak2010@gmail.com>
2016-11-05 03:40:21 +0000

    refactor: Chart data are sorted and build in controller and not helper

--- app/controllers/key-metrics.js
+++ app/controllers/key-metrics.js
@@ -31,5 +31,31 @@ export default Ember.Controller.extend({
     Ember.run.schedule('afterRender', () => {
       componentHandler.upgradeAllRegistered();
     });
+  },
+
+  lineChartData: function () {
+    let sortedData = this.get('model').customers.sortBy('timestamp');
+    let formatedChartData = [];
+    sortedData.forEach((item) => {
+      formatedChartData.push([
+        new Date(item.timestamp),
+        item.paying_customers
+      ]);
+    });
+    return formatedChartData;
+  }.property('model.customers.@each'),
+
+  barChartData: function () {
+    console.log(this.get('model').issues);
+    let sortedData = this.get('model').issues.sortBy('timestamp');
+    let formatedChartData = [];
+    sortedData.forEach((item) => {
+      formatedChartData.push([
+        new Date(item.timestamp),
+        item.issues
+      ]);
+    });
+    return formatedChartData;
+  }.property('model.issues.@each'),
   }
 });
