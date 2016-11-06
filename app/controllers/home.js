545daf8aa57eaf64cc2e2fc69b3f92fb1075d518
Peter Janak <2010janak2010@gmail.com>
2016-11-05 23:35:58 -0800

    refactor: Use ember`s property method instead to convert home page data

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,26 +3,25 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
   dataConverter: Ember.inject.service('data-converter'),
-  numberOfEmployees: 0,
 
   init () {
     this.set('apiKey', config.dashboardAppGoogleApiKey);
-
-    Ember.run.schedule('afterRender', () => {
-      const data = this.get('model');
-      const dataArray = this.get('dataConverter').csvToArray(data);
-      this.set('dataArray', dataArray);
-      this.set('numberOfEmployees', this.getNumberOfEmployees(dataArray));
-    });
   },
 
-  getNumberOfEmployees (data) {
+  dataArray: function () {
+    const model = this.get('model');
+    return this.get('dataConverter').csvToArray(model);
+  }.property('model'),
+
+  numberOfEmployees: function () {
+    const dataArray = this.get('dataArray');
+
     let result = 0;
-    data.forEach((country) => {
+    dataArray.forEach((country) => {
       if (typeof country[1] === 'number') {
         result += country[1];
       }
     });
     return result;
-  }
+  }.property('dataArray')
 });
