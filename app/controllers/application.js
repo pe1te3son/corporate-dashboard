5678813dc503416fb87834aca2999592fd1291fb
Peter Janak <2010janak2010@gmail.com>
2016-11-06 23:21:49 -0800

    feat: Change title for each page dynamicly in application controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Controller.extend({
   init () {
     Ember.run.schedule('afterRender', () => {
       componentHandler.upgradeAllRegistered();
+      $('.main-nav-link').click(function () {
+        $('#current-page-title').text(
+          $(this).children('.main-nav-link-name').text()
+        );
+      });
     });
   }
 });
