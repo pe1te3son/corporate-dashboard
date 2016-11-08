4e10eb91b8169108ae36347c798ad38c65d2f87a
Peter Janak <2010janak2010@gmail.com>
2016-11-08 02:26:25 +0000

    refactor: Remove number of employees info card

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,7 +3,6 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
   dataConverter: Ember.inject.service('data-converter'),
-  numberOfEmployees: 0,
 
   init () {
     this.set('apiKey', config.dashboardAppGoogleApiKey);
@@ -12,17 +11,6 @@ export default Ember.Controller.extend({
       const data = this.get('model');
       const dataArray = this.get('dataConverter').csvToArray(data);
       this.set('dataArray', dataArray);
-      this.set('numberOfEmployees', this.getNumberOfEmployees(dataArray));
     });
-  },
-
-  getNumberOfEmployees (data) {
-    let result = 0;
-    data.forEach((country) => {
-      if (typeof country[1] === 'number') {
-        result += country[1];
-      }
-    });
-    return result;
   }
 });
