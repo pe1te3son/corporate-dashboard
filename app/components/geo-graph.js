a881c6368cda34c5e2e0034e008a1dce3792e018
Peter Janak <2010janak2010@gmail.com>
2016-11-09 03:08:01 +0000

    refactor: Remove polling service from geo-graph component

--- app/components/geo-graph.js
+++ app/components/geo-graph.js
@@ -1,49 +1,16 @@
 import Ember from 'ember';
-import $ from 'jquery';
 
 /**
  * @name Geo-graph Component
- * @desc Displays map with employess in different locations. Simulates data
- * update. Uses polling service to refetch required data
- * @requires poller service
+ * @desc Displays map with employess in different locations.
 */
 export default Ember.Component.extend({
-  poller: Ember.inject.service('poller'),
-  dataConverter: Ember.inject.service('data-converter'),
-  chartData: null,
-
-  init () {
-    const _this = this;
-    this._super();
-    this.get('poller').onPoll = function () {
-      return _this.drawChart();
-    };
-  },
-  drawChart () {
-    const _this = this;
-    return $.get(_this.get('dataSrc'))
-      .then(response => {
-        return _this.get('dataConverter').csvToArray(response);
-      })
-      .then(respConverted => {
-        _this.updateValues(respConverted);
-        _this.set('chartData', respConverted);
-      });
-  },
-
-  updateValues (array) {
-    this.dataArray = array;
-    for (var i = 1; i < this.dataArray.length; i++) {
-      let randomNumber = Math.floor(Math.random() * (499 + 1)) + 1;
-      this.dataArray[i][1] += randomNumber;
-    }
-    return this.dataArray;
-  },
 
   chartDatahasChanged: function () {
-    let data = google.visualization.arrayToDataTable(this.get('chartData'));
+    console.log('dataChanged');
+    let data = google.visualization.arrayToDataTable(this.get('data'));
     this.get('chart').draw(data);
-  }.observes('chartData'),
+  }.observes('data'),
 
   didInsertElement () {
     const _this = this;
@@ -54,13 +21,8 @@ export default Ember.Component.extend({
 
     function setChart () {
       _this.set('chart', new google.visualization.GeoChart(document.getElementById(elId)));
-      _this.drawChart();
+      let data = google.visualization.arrayToDataTable(_this.get('data'));
+      _this.get('chart').draw(data);
     }
-
-    this.get('poller').startPolling();
-  },
-
-  willDestroyElement () {
-    this.get('poller').stopPolling();
   }
 });
