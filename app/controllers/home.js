6e45ee05ef42e471cd4efaa0297155544f5b7e99
Peter Janak <2010janak2010@gmail.com>
2016-11-09 03:12:23 +0000

    feat: Set basic polling method on home page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import config from '../config/environment';
+import $ from 'jquery';
 
 /**
  * @name homeController
@@ -11,5 +12,44 @@ export default Ember.Controller.extend({
 
   init () {
     this.set('apiKey', config.dashboardAppGoogleApiKey);
+  },
+
+  intervalDefault: 5000,
+
+  schedulePollEvent (event, interval) {
+    var eventInterval = interval || this.get('intervalDefault');
+    return Ember.run.later(() => {
+      event.apply(this);
+      this.set('timer', this.schedulePollEvent(event));
+    }, eventInterval);
+  },
+
+  startPolling (interval) {
+    this.set('timer', this.schedulePollEvent(this.get('onPoll'), interval));
+  },
+
+  stopPolling () {
+    Ember.run.cancel(this.get('timer'));
+  },
+
+  onPoll () {
+    const _this = this;
+    return $.get('data/employees.csv')
+      .then((response) => {
+        return _this.get('dataConverter').csvToArray(response);
+      })
+      .then(array => {
+        // Simulate changed data
+        _this.set('model', _this.updateValues(array));
+      });
+  },
+
+  updateValues (dataArray) {
+    for (var i = 1; i < dataArray.length; i++) {
+      let randomNumber = Math.floor(Math.random() * (499 + 1)) + 1;
+      dataArray[i][1] += randomNumber;
+    }
+    return dataArray;
   }
+
 });
