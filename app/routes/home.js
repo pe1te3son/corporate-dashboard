e4d1ca61019ebfe14d7d5ae01b354a2c7b06b6ad
Peter Janak <2010janak2010@gmail.com>
2016-11-09 03:14:21 +0000

    feat: Fetch and convert data for home page geo graph

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
+  dataConverter: Ember.inject.service('data-converter'),
+
+  model () {
+    return $.get('data/employees.csv').then(response => {
+      return this.get('dataConverter').csvToArray(response);
+    });
+  },
+
   afterModel: function () {
     this.controllerFor('home').startPolling();
     Ember.run.later(() => {
