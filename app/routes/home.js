65b39dfed26d89fb5ded7517b67d0b42fbd9f72e
Peter Janak <2010janak2010@gmail.com>
2016-11-09 03:13:46 +0000

    feat: Activate and deactivate polling on home route change

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel: function () {
+    this.controllerFor('home').startPolling();
+    Ember.run.later(() => {
+
+    }, 2000);
+  },
+
+  deactivate: function () {
+    this.controllerFor('home').stopPolling();
+  }
 });
