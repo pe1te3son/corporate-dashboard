afca5b6fdbe227479ec921a211b33d2958ff2eff
Peter Janak <2010janak2010@gmail.com>
2016-11-10 03:11:51 +0000

    feat: Set polling for key-metrics page

--- app/controllers/key-metrics.js
+++ app/controllers/key-metrics.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 /**
  * @name key-metrics Controller
  * @desc Key-metrics page Controller
 */
 export default Ember.Controller.extend({
+  pollerService: Ember.inject.service('poller'),
   currentIssues: null,
 
   // Paying cutomers chart options
@@ -43,6 +45,7 @@ export default Ember.Controller.extend({
     });
   },
 
+  // Customers
   lineChartData: function () {
     let sortedData = this.get('model').customers.sortBy('timestamp');
     let formatedChartData = [];
@@ -55,8 +58,8 @@ export default Ember.Controller.extend({
     return formatedChartData;
   }.property('model.customers.@each'),
 
+  // Issues
   barChartData: function () {
-    console.log(this.get('model').issues);
     let sortedData = this.get('model').issues.sortBy('timestamp');
     let formatedChartData = [];
     sortedData.forEach((item) => {
@@ -68,13 +71,60 @@ export default Ember.Controller.extend({
     return formatedChartData;
   }.property('model.issues.@each'),
 
-  actions: {
-    chartHasUpdated (chartUpdateData) {
-      if (chartUpdateData.chartType === 'Bar') {
-        // Display updated number of issues on page
-        this.set('currentIssues', chartUpdateData.payload);
-      }
-    }
-  }
+  intervalDefault: 5000,
+
+  schedulePollEvent (event, interval) {
+    var eventInterval = interval || this.get('intervalDefault');
+    return Ember.run.later(() => {
+      event.apply(this);
+      this.set('timer', this.schedulePollEvent(event));
+    }, eventInterval);
+  },
+
+  startPolling (interval) {
+    this.set('timer', this.schedulePollEvent(this.get('onPoll'), interval));
+  },
+
+  stopPolling () {
+    Ember.run.cancel(this.get('timer'));
+  },
+
+  onPoll () {
+    $.get('data/number_of_paying_customers_per_year.json')
+      .then(response => {
+        // Update values
+        return this.updateValues(response, 'paying_customers');
+      })
+      .then(resUpdated => {
+        this.set('model.customers', resUpdated);
+      });
+
+    $.get('data/number_of_issues.json')
+      .then(response => {
+        // Update values
+        return this.updateValues(response, 'issues');
+      })
+      .then(resUpdated => {
+        this.set('model.issues', resUpdated);
+      });
+  },
+
+  updateValues (array, property) {
+    const arrayData = array;
+    const min = 50;
+    const max = 1000;
+    arrayData.forEach((item) => {
+      item[property] += Math.floor(Math.random() * (max - min + 1)) + min;
+    });
+    return arrayData;
+  },
+
+  issuesModelHasChanged: function () {
+    let issuesSum = 0;
+    this.get('model').issues.forEach((issue) => {
+      issuesSum += issue.issues;
+    });
+    this.set('currentIssues', issuesSum);
+  }.observes('model.issues')
 
 });
