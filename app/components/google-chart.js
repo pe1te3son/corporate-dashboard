8112494cd491470401f1a163f7b62c65eac6b86c
Peter Janak <2010janak2010@gmail.com>
2016-11-10 03:10:21 +0000

    feat: Set component global function to draw chart

--- app/components/google-chart.js
+++ app/components/google-chart.js
@@ -16,32 +16,45 @@ export default Ember.Component.extend({
 
   didInsertElement () {
     const elId = this.$().attr('id');
-    this.set('elId', elId);
-    const formatedChartData = this.get('data');
     const _this = this;
 
     this.setChartWidth();
-    google.charts.setOnLoadCallback(draw);
+    google.charts.setOnLoadCallback(function () {
+      _this.drawChart(elId, _this.get('data'));
+    });
 
-    // Draw chart
-    function draw () {
-      // Set chart globaly within component to be reused
-      _this.set('chartData', new google.visualization.DataTable());
-      _this.get('chartData').addColumn('date', _this.get('xAxisLegend'));
-      _this.get('chartData').addColumn('number', _this.get('yAxisLegend'));
-      _this.get('chartData').addRows(formatedChartData);
+    // // Draw chart
+    // function draw () {
+    //   // Set chart globaly within component to be reused
+    //   _this.set('chartData', new google.visualization.DataTable());
+    //   _this.get('chartData').addColumn('date', _this.get('xAxisLegend'));
+    //   _this.get('chartData').addColumn('number', _this.get('yAxisLegend'));
+    //   _this.get('chartData').addRows(_this.get('data'));
+    //
+    //   _this.set(
+    //     'chart',
+    //     new google.charts[_this.get('chartType')](document.getElementById(elId))
+    //   );
+    //   _this.get('chart').draw(_this.get('chartData'), _this.get('chartOptions'));
+    // }
+  },
 
-      _this.set(
-        'chart',
-        new google.charts[_this.get('chartType')](document.getElementById(elId))
-      );
-      _this.get('chart').draw(_this.get('chartData'), _this.get('chartOptions'));
-    }
+  drawChart (elId, chartData) {
+    this.set(
+      'chart',
+      new google.charts[this.get('chartType')](document.getElementById(elId))
+    );
+    const data = new google.visualization.DataTable();
+    data.addColumn('date', this.get('xAxisLegend'));
+    data.addColumn('number', this.get('yAxisLegend'));
+    data.addRows(chartData);
+    this.get('chart').draw(data, this.get('chartOptions'));
   },
 
   didRender () {
     // If window resized redraw chart
     const _this = this;
+    const elId = this.$().attr('id');
     $(window).resize(function () {
       if (this.isResizing) {
         clearTimeout(this.isResizing);
@@ -54,17 +67,30 @@ export default Ember.Component.extend({
 
     $(window).on('hasResized', function () {
       _this.setChartWidth();
-      _this.get('chart').draw(_this.get('chartData'), _this.get('chartOptions'));
+      _this.drawChart(elId, _this.get('data'));
     });
   },
 
   didUpdateAttrs () {
-    console.log('attr update');
-    console.log(this.get('chartType'));
+    if (this.get('chart')) {
+      Ember.run.schedule('afterRender', () => {
+        this.drawChart(
+          this.$().attr('id'),
+          this.get('data')
+        );
+      });
+    }
   },
+
   // Set chart width to 100% width of component
   setChartWidth () {
     const elWidth = $(`#${this.get('elId')}`).width();
     this.get('chartOptions').width = elWidth - 15;
+  },
+
+  didDestroyElement () {
+    // Remove custom listeners
+    $(window).off('resize');
+    $(window).off('hasResized');
   }
 });
